3cdcc6c95ede55370ce7595c4788f5b1a314ea4b
espoupou <espoirativon@gmail.com>
2023-03-17 17:50:15 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,12 +10,12 @@
 
 int main(void)
 {
-	char c;
+	char c, d;
 
 	for (c = 'a'; c <= 'z'; c++)
 		putchar(c);
-        for (c = 'A'; c <= 'Z'; c++)
-                putchar(c);
+        for (d = 'A'; d <= 'Z'; d++)
+                putchar(d);
 	putchar('\n');
 	return (0);
 }
