ea9570bb26652ca435a914447a7e44331bc7b18e
espoupou <espoirativon@gmail.com>
2023-03-21 14:22:50 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - test a number and print it sign
+ *
+ * @n: the value to test
+ * Return: 
+ */
+
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
