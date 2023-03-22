932c977c0b5abcf8da789c86dc207b1996048fbd
espoupou <espoirativon@gmail.com>
2023-03-21 20:01:34 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,10 +10,25 @@
 
 int print_last_digit(int n)
 {
+	int l;
+
+	l = n % 10;
+	if (l < 0)
+	{
+		_putchar(-l + 48);
+		return (-l);
+	}
+	else
+	{
+		_putchar(l + 48);
+		return (l);
+	}
+/*
 	if (n < 0)
 	{
 		n = -n;
 	}
 	_putchar('0' + (n % 10));
 	return (n % 10);
+*/
 }
