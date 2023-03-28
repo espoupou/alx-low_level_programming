491b38219f4ac358b8a7777dad0f4973d8ac0f1a
espoupou <espoirativon@gmail.com>
2023-03-27 17:54:00 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string.
+ * @s: input string.
+ * Return: no return.
+ */
+
+void rev_string(char *s)
+{
+	int c = 0, i, j;
+	char temp;
+
+	while (s[c] == '\0')
+	{
+		c++;
+	}
+
+	for (i = 0; i < (c / 2); i++)
+	{
+		temp = *(s + i);
+		*(s + i) = *(s + c - i - 1);
+		*(str + c - i - 1) = temp;
+	}
+}
