3aadc9f094eba1cd59d3839425326aa1c2be656e
espoupou <espoirativon@gmail.com>
2023-03-27 19:01:29 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copy the string src to dest
+ * @dest: destination.
+ * @src: source.
+ * Return: the pointer to dest.
+ */
+
+char * _strcpy(char *dest, char *src)
+{
+	int c = 0;
+
+	while (* (src + c) != '\0')
+	{
+		*(dest + c) = *(src + c);
+		c++;
+	}
+	return (dest);
+}
