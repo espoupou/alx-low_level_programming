620eddbaa1cb60a557780e9c0553b5c18dc0834f
espoupou <espoirativon@gmail.com>
2023-03-30 16:58:30 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,9 +10,11 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int i = 0, j;
 
-	for (i = 0; *(dest + i) != '\0'; i++);
+	while (*(dest + i) != '\0')
+		i++;
+
 	for (j = 0; *(src + j) != '\0' && j < n; j++)
 	{
 		*(dest + i + j) = *(src + j);
