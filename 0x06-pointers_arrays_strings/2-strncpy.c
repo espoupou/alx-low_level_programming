fc1e01fc3fc42b50d36d698bfb1a5bf400b8240b
espoupou <espoirativon@gmail.com>
2023-03-30 17:03:07 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncat - concatanate n bit of src to dest
+ * @dest: destination string
+ * @src: source string
+ * @n: the number of bit
+ * Return: the concatanated string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j;
+
+	while (*(dest + i) != '\0')
+		i++;
+
+	for (j = 0, i--; *(src + j) != '\0' && j < n; j++)
+	{
+		*(dest + i + j) = *(src + j);
+	}
+	*(dest + j) = '\0';
+
+	return (dest);
+}
