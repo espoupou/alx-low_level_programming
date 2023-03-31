14ea6ff61941933fe52aca6241aa64180e750122
espoupou <espoirativon@gmail.com>
2023-03-30 17:12:21 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,9 +9,10 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s1[i] && s2[i] && s1[i] == s2[i]; i++);
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
 
 	if (s1[i] == s2[i])
 	{
