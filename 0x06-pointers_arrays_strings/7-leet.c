452181c2a6b5b1535601efc8be3ca5e5c4e44664
espoupou <espoirativon@gmail.com>
2023-03-30 17:19:12 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - leet conversion
+ * @s: the string
+ * Return: the leet converted
+ */
+
+char *leet(char *s)
+{
+	char c[] = "aAeEoOtTlL";
+	char l[] = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; c[j] != '\0'; j++)
+		{
+			if (s[i] == c[j])
+			{
+				s[i] = l[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
+
+
