58cb61e5c818b516a7d8fe65ad23aa6730f1cd37
espoupou <espoirativon@gmail.com>
2023-03-30 17:33:51 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+ * check - check separator
+ * @s: the string
+ * @i: position
+ * Return: 0 or 1
+ */
+
+int check(char *s, int i)
+{
+	return (s[i - 1] == ' ' || s[i - 1] == ',' || s[i - 1] == ';' ||
+		s[i - 1] == '.' || s[i - 1] == '!' || s[i - 1] == '?' ||
+		s[i - 1] == '"' || s[i - 1] == '(' || s[i - 1] == ')' ||
+		s[i - 1] == ',' || s[i - 1] == '{' || s[i - 1] == '}' ||
+		(s[i - 1] == 't' && s[i - 2] == '\\') ||
+		(s[i - 1] == 'n' && s[i - 2] == '\\'));
+}
+
+/**
  * cap_string - capitalize
  * @s: the string
  * Return: Capitalized
@@ -12,7 +29,7 @@ char *cap_string(char *s)
 
 	while (s[i] != '\0')
 	{
-		if (s[i] >= 'a' && s[i] <= 'z')
+		if (s[i] >= 'a' && s[i] <= 'z' && check(s, i))
 			s[i] = s[i] + 'A' - 'a';
 		i++;
 	}
