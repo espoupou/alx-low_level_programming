53c36f174318df6963bd9406c8dd5a7b182bd278
espoupou <espoirativon@gmail.com>
2023-04-03 14:32:20 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,37 @@
+#include <string.h>
+
+/**
+ * _strstr - locates a substring
+ * @haystack: the string
+ * @needle: the substring 
+ *
+ * Return:  first occurrence pointer or null
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int index;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		index = 0;
+
+		if (haystack[index] == needle[index])
+		{
+			do {
+				if (needle[index + 1] == '\0')
+					return (haystack);
+
+				index++;
+
+			} while (haystack[index] == needle[index]);
+		}
+
+		haystack++;
+	}
+
+	return ('\0');
+}
