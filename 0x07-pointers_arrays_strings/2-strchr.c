e060e77c76050533c2dae2f2245cad5cfe727541
espoupou <espoirativon@gmail.com>
2023-04-03 14:02:24 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr -  locates a character in a string
+ * @s: the string
+ * @c: the character
+ * Return: pointer to first occurence or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *s[i] && s[i] != c; i++)
+		;
+
+	if (s[i] == c)
+		return (s + i);
+	return (NULL);
+}
