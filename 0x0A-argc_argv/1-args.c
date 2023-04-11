b45d78f1a881fe327cfe257729096ab0927ce568
espoupou <espoirativon@gmail.com>
2023-04-10 17:04:35 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print arguments count
+ * @argc: arguments count
+ * @argv: arguments
+ * Return: always 0
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
