1a5eb1b1ef73004d4938241d765137bfd37cae84
espoupou <espoirativon@gmail.com>
2023-04-10 17:08:49 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define UNUSED(x) (void)(x)
 
 /**
  * main - print arguments count
@@ -9,6 +10,7 @@
 
 int main(int argc, char **argv)
 {
+	UNUSED(argv);
 	printf("%d\n", argc);
 	return (0);
 }
