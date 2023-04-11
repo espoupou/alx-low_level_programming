9b1ff5978d3dbdd77897d6d8158bf73204c899a9
espoupou <espoirativon@gmail.com>
2023-04-10 17:07:07 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define UNUSED(x) (void)(x)
 
 /**
  * main - print program name
@@ -9,6 +10,7 @@
 
 int main(int argc, char **argv)
 {
+	UNUSED(argc);
 	printf("%s\n", argv[0]);
 	return (0);
 }
