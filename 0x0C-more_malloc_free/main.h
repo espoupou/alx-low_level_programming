1e54a349fcb9e2fe2c2969ccc0e8af330194c8eb
espoupou <espoirativon@gmail.com>
2023-04-14 15:54:39 -0700

    101-mul

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,7 +2,9 @@
 #define __MAIN_H__
 
 #include <stdlib.h>
+#include <stddef.h>
 
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
