c96aa47d017db03bb21032cf5892bbdedd6d21ad
espoupou <espoirativon@gmail.com>
2023-05-02 10:22:11 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 -  frees a listint_t list
+ * @head: the list
+ * Return: nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	while ((current = *head) != NULL)
+	{
+		*head = current->next;
+		free(current);
+	}
+	*head = NULL;
+}
