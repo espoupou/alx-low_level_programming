61eb67afe70b6fc96efeecd91783a883a4c01f2e
espoupou <espoirativon@gmail.com>
2023-05-02 10:09:54 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * listint_len - number of elements
+ * @h: the list
+ * Return: the size
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t s = 0;
+
+	while (h != NULL)
+	{
+		s++;
+	}
+	return (s);
+}
