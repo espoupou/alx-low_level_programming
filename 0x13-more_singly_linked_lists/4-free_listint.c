bdbad28d449ab3b1acd9718999f814080c0efd3f
espoupou <espoirativon@gmail.com>
2023-05-02 10:16:32 -0700

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: the list
+ * Return: nothing
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t current = head;
+
+	while ((current = head) != NULL)
+	{
+		head = head->next;
+		free(current);
+	}
+}
