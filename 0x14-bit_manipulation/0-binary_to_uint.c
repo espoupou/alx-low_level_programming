b2bbb0d8f08626ac7ac3b74f28d77b47a39057d8
espoupou <espoirativon@gmail.com>
2023-05-04 17:32:42 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,30 +8,28 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int u;
-	int i = 1;
-	char *p;
+	unsigned int ui;
+	int len, base_two;
 
 	if (!b)
 		return (0);
 
-	u = 0;
+	ui = 0;
 
-	for (p = b; *p != '\0'; p++)
+	for (len = 0; b[len] != '\0'; len++)
 		;
 
-	for (p--; p != b - 1; i *= 2)
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if (*p != '0' && *p != '1')
+		if (b[len] != '0' && b[len] != '1')
 		{
 			return (0);
 		}
 
-		if (*p & 1)
+		if (b[len] & 1)
 		{
-			u += i;
+			ui += base_two;
 		}
 	}
-
-	return (u);
+	return (ui);
 }
