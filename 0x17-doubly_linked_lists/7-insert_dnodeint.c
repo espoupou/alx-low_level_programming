10f920cb5f11ccc6b029995df6c783b88a5bb807
espoupou <espoirativon@gmail.com>
2023-06-16 11:47:14 -0700

    betty

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -27,7 +27,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (new == NULL)
 			return (NULL);
 		if (head == NULL)
-			return add_dnodeint_end(h, n);
+			return (add_dnodeint_end(h, n));
 		new->n = n;
 		new->next = head;
 		new->prev = head->prev;
