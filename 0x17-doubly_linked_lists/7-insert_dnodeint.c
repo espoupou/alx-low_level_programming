be5fa30a558f894c477aa97a662d24e58468b2e9
espoupou <126259211+espoupou@users.noreply.github.com>
2023-06-16 19:49:44 +0100

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -34,6 +34,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new->prev->next = new;
 		new->next->prev = new;
 	}
+	else if (head == NULL && idx == 1)
+		return (add_dnodeint_end(h, n));
 
 	return (new);
 }
